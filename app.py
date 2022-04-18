26e83d0129c348dff385b45df5bd9fd788d0a12b
Corin-M <corin.magee@uni.minerva.edu>
2022-04-18 22:28:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,74 @@
 import os
-from base64 import urlsafe_b64decode
 from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Boolean
 
-UPLOAD_FOLDER = 'templates/uploads'
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///db.sqlite3'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
-app.config['UPLOAD_FOLDER']=UPLOAD_FOLDER
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Upload(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    filename = db.Column(db.String(50))
-    fileurl= db.Column(db.String(50))
+    fileName = db.Column(db.String(50))
+    fileURL= db.Column(db.String(50))
+    tags= db.Column(db.String(50))
+  #  medical= db.Column(db.String(16))
+   # errands=db.Column(db.String(16))
+  #  emotReg = db.Column(db.String(16))
+    audience= db.Column(db.String(50))
+
 db.create_all()
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/')
 def index():
-  
-        upload = Upload(filename=file.filename, fileurl=(app.config['UPLOAD_FOLDER']+file.filename))
-        db.session.add(upload)
-        db.session.commit()
     displayVids = []
-    for file in Upload.query.all():
-        displayVids.append(file)
+    for video in Upload.query.all():
+        displayVids.append([video.fileName,video.fileURL])
     return render_template('index.html', vids=displayVids)
 
+@app.route('/filter', methods=['POST'])
+def filter():
+    displayVids = []
+    filter=request.form['filter-method']
+    if filter == "all":
+      return redirect('/')
+    else:
+        for video in Upload.query.filter(Upload.tags.contains(filter)):
+            displayVids.append([video.fileName,video.fileURL])
+    return render_template('index.html', vids=displayVids)
 
+@app.route('/uploads', methods=['GET','POST'])
+def uploads():
+    return render_template('uploading.html')
+
+@app.route('/add', methods=['GET','POST'])
+def add():
+        fullURL = request.form["videoURL"]
+        startNumIndex = fullURL.find("v=")
+        if startNumIndex == -1 or "youtube" not in fullURL:
+            flash('Not valid URL try again')
+            return redirect('/uploads')
+        tagList=request.form.getlist('tags')
+        tags=""
+        for tag in tagList:
+            tags = tags +tag
+        urlID= "https://www.youtube.com/embed/"+ fullURL[startNumIndex+2:startNumIndex +13]
+        newUpload = Upload(fileName=request.form['videoName'], fileURL=urlID, audience=request.form['audience'], 
+                   tags= tags)
+      #  newUpload = Upload(fileName=request.form['videoName'], fileURL=urlID, audience=request.form['audience'], 
+      #                      school = request.form['school'],
+      #                      medical= request.form['medical'],
+      #                      errands= request.form['errands'],
+      #                      emotReg=request.form['emotReg'])
+        db.session.add(newUpload)
+        db.session.commit()
+        return redirect('/')
+        
+@app.route('/howTo')
+def howTo():
+    return render_template("howTo.html")
 
 if __name__== "__main__":
     db.create_all()
