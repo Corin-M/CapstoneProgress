c8630a9101b87f34b2d5f4f836d4b4cac5398445
Corin-M <corin.magee@uni.minerva.edu>
2022-10-26 13:02:11 -0700

    still fixing heroku

--- app.py
+++ app.py
@@ -163,5 +163,5 @@ def resourcesPage():
 #Flask boot up commands for deployment
 if __name__== "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(port=port,debug= True)
+    app.run(host='0.0.0.0',port=port,debug= True)
 
