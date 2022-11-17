dc78a8f2d02c7e5f603cbfc49d0bd9db1f0c2b2f
Corin-M <corin.magee@uni.minerva.edu>
2022-11-17 12:04:17 -0800

    Changed app host settings for vercel

--- app.py
+++ app.py
@@ -170,7 +170,6 @@ def resourcesPage():
 
 #Flask boot up commands for deployment
 if __name__== "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0',port=port)
+    app.run()
 
     
